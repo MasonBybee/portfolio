be65b9651d555e88ff3f58ad3ea06afadbc8c03a
Mason Bybee <bybeemason@gmail.com>
2024-04-23 19:17:08 -0500

    moved card data to objects

---
+++ cards.py
@@ -0,0 +1,35 @@
+work_cards = [
+    {
+        "title": "Pour & Explore",
+        "img_src": "/static/assets/Portfolio/pour&Explore.PNG",
+        "img_alt": "Picture of Pour & Explore cocktail finder",
+        "website_link_title": "Explore",
+        "website_link": "http://172.232.10.66/",
+        "github_link": "https://github.com/MasonBybee/Cocktail-Capstone",
+        "text": "Find new and exciting cocktails with my first capstone project!",
+    }
+]
+
+fun_cards = [
+    {
+        "title": "Lights Out",
+        "img_src": "/static/assets/Portfolio/lightsOut.PNG",
+        "img_alt": "Picture of Lights Out Game",
+        "github_link": "https://github.com/MasonBybee/lightsout",
+        "website_link": "https://masonbybee.github.io/lightsout/",
+    },
+    {
+        "title": "Pygame Snake(WIP)",
+        "img_src": "/static/assets/Portfolio/snake.PNG",
+        "img_alt": "Picture of Pygame Snake Game",
+        "github_link": "https://github.com/MasonBybee/Snake",
+        "website_link": "",
+    },
+    {
+        "title": "Connect Four",
+        "img_src": "/static/assets/Portfolio/connectfour.PNG",
+        "img_alt": "Picture of Connect Four",
+        "github_link": "https://github.com/MasonBybee/Connect4",
+        "website_link": "https://masonbybee.github.io/Connect4/",
+    },
+]
