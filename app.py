96ad4dee753090b571131e7be98eeeb2043bc78d
Mason Bybee <bybeemason@gmail.com>
2024-04-23 19:16:51 -0500

    added card data import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect
+from cards import work_cards, fun_cards
 
 app = Flask(__name__)
 
@@ -8,4 +9,4 @@ app.config["SECRET_KEY"] = "its_a_secret"
 
 @app.route("/")
 def show_home():
-    return render_template("index.html")
+    return render_template("index.html", work_cards=work_cards, fun_cards=fun_cards)
